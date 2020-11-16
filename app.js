5910324b5404799d86a6912e8f1822cdf8c0b479
spaceroot <denis3.14159@gmail.com>
2020-11-16 15:00:06 +0100

    fix

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.get("/", function(req, res) {
 	res.send(index);
 });
 
+<<<<<<< Updated upstream
+=======
+app.get("/api/combinations", function(req, res) {
+	res.json(combinations);
+});
+
+app.post("/api/combinations", function(req, res) {
+	res.status(201).json(req.body);
+})
+
+>>>>>>> Stashed changes
 app.listen(process.env.PORT || 3000, function() {
 	console.log("Lotoclub has started");
 });
