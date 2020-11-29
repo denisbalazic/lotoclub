c4cd935fca372e61a3c8f733b0f63a6660059fd1
spaceroot <denis3.14159@gmail.com>
2020-11-29 18:13:42 +0100

    bug fix

--- public/js/combinations.js
+++ public/js/combinations.js
@@ -28,7 +28,6 @@ async function fetchCombinations() {
   try {
     const res = await fetch("http://localhost:3000/api/combinations/");
     const data = await res.json();
-    console.log(data);
     return data;
   } catch (err) {
     console.log(err);
